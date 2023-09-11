a5c1fcb032735d33406604c0374506ffab2105f3
Nikith Kalu <nikithkalu@gmail.com>
2023-09-11 16:24:59 +1000

    initial commit
    SIT120 1-1P

---
+++ script.js
@@ -0,0 +1,4 @@
+function submitForm(event) {
+  event.preventDefault(); // Prevent the form from submitting due to scope of solution
+  window.location.href = "thank_you.html"; // Redirect to thank_you.html
+}
