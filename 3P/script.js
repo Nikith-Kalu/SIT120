2bdd2a14e0f9135811ff97a47da5f55ec97593bb
Nikith Kalu <nikithkalu@gmail.com>
2023-10-06 22:12:15 +1100

    3P

---
+++ 3P/script.js
@@ -0,0 +1,14 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      // Vue data properties will go here
+    };
+  },
+  methods: {
+    // Vue methods will go here
+  },
+});
+
+// Vue components and Vue Router setup will be added here
+
+app.mount("#app");
